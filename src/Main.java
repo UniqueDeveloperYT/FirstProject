733ae6cd3586750ba9a21e0c5aed4b60b147989a
Sunil Yadav <sam123.sy28@gmail.com>
2024-11-21 21:29:22 +0530

    initial commit

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,20 @@
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.function.Consumer;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
 public class Main {
+    static int i = 0;
     public static void main(String[] args) {
 
-        System.out.println("Hello world!");
-        System.out.println("Second commit");
+        int[] numbers = {2,4,4,53,45,45,645,6,456};
+        Arrays.stream(numbers)
+                .filter(num->num%2==0)
+                .forEach(System.out :: println);
+
+
     }
 }
